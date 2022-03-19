dab1488e939b03d0e5ac83f773aa2f56a4065237
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-19 22:54:31 +0000

    Componentize pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import { RiAddLine, RiEditLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
-import Pagination from "../../components/Pagination";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
