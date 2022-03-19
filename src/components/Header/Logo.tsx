6dca1ac8be8212024bcd63ebd2dfaeab7da16313
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-19 23:05:51 +0000

    Responsiveness with chakra js

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,8 +2,14 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
     return (
+        // in order to deal with responsiviness, chakra allows an array of sizes to be provided. The first element is for the smallest breakpoint (or UI)
+        // the second for the one after, etc. In case a size is not provided for each breakpoint the last provided one will be used for all
+        // the bigger breakpoints. https://chakra-ui.com/docs/styled-system/features/responsive-styles
+        // Example 1:  fontSize={['2xl', '3xl']}  - 2xl will be used for sm screens and 3xl for md onwards
+        // Example 2: fontSize={['2xl', '3xl', 5xl]} - 2xl for sm, 3xl for md and 5xl for lg onwards.
+        // It is possible to define custom breakpoints on top of the default ones defined by chakra
         <Text 
-            fontSize='3xl' 
+            fontSize={['2xl', '3xl']} 
             fontWeight='bold' 
             letterSpacing='tight'
             w='64'
