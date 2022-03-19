ae1b28bd6201b24841ff114475c031fa38075940
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-19 17:18:56 +0000

    Create dashboard main content structure

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
@@ -9,6 +9,23 @@ export default function Dashboard() {
 
             <Flex w='100%' my='6' maxWidth='1480px' mx='auto' px='6'>
                 <Sidebar />
+
+                <SimpleGrid flex='1' gap='4' minChildWidth='320px'>
+                    <Box 
+                        p='8'
+                        bg='gray.800'
+                        borderRadius='8'
+                    >
+                        <Text fontSize='lg' mb='4'>Weekly subscriptions</Text>
+                    </Box>
+                    <Box
+                        p='8'
+                        bg='gray.800'
+                        borderRadius='8'
+                    >
+                        <Text fontSize='lg' mb='4'>Opening rate</Text>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     );
