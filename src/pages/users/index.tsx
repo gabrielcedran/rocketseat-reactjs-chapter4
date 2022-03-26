3cdf822678312c0e06ed90afc0781c8dfcc81ffe
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-26 20:30:24 +0000

    Improve responsiveness of the users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpoint, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiEditLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideScreen = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -29,15 +34,17 @@ export default function UserList() {
                     <Table colorScheme='whiteAlpha'>
                         <Thead>
                             <Tr>
-                                <Th px='6' color='gray.300' width='8'>
+                                <Th px={['4','4','6']} color='gray.300' width='8'>
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Th>
                                 <Th>
                                     User
                                 </Th>
-                                <Th>
+                                {isWideScreen && 
+                                (<Th>
                                     Creation Date
                                 </Th>
+                                )}
                                 <Th w='6'>
 
                                 </Th>
@@ -45,7 +52,7 @@ export default function UserList() {
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px='6'>
+                                <Td px={['4','4','6']}>
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Td>
                                 <Td>
@@ -54,15 +61,16 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'>don@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>
+                                {isWideScreen && 
+                                (<Td>
                                     April 21st, 2021
-                                </Td>
+                                </Td>)}
                                 <Td>
-                                    <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiEditLine}/>}>Edit</Button>
+                                    <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiEditLine}/>}>{isWideScreen ? 'Edit' : ''}</Button>
                                 </Td>
                             </Tr>
                             <Tr>
-                                <Td px='6'>
+                                <Td px={['4','4','6']}>
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Td>
                                 <Td>
@@ -71,11 +79,12 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'>bob@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>
+                                {isWideScreen && 
+                                (<Td>
                                     April 21st, 2021
-                                </Td>
+                                </Td>)}
                                 <Td>
-                                    <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiEditLine}/>}>Edit</Button>
+                                    <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiEditLine}/>}>{isWideScreen ? 'Edit' : ''}</Button>
                                 </Td>
                             </Tr>
                         </Tbody>
