c0146182fab56d7e8b41e1775062a315631d12b1
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-26 20:33:23 +0000

    Improve responsiveness of the create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -19,7 +19,7 @@ export default function CreateUser() {
                     flex='1' 
                     borderRadius={8} 
                     bg='gray.800' 
-                    p='8'
+                    p={['6','8']}
                 >
                     <Heading 
                         size='lg' 
@@ -35,7 +35,7 @@ export default function CreateUser() {
                     <VStack spacing='8'>
                         <SimpleGrid 
                             minChildWidth='240px' 
-                            spacing='8' 
+                            spacing={['6','8']} 
                             w='100%'
                         >
                             <Input name='name' label='Full name'/>
@@ -43,7 +43,7 @@ export default function CreateUser() {
                         </SimpleGrid>
                         <SimpleGrid 
                             minChildWidth='240px' 
-                            spacing='8' 
+                            spacing={['6','8']}
                             w='100%'
                         >
                             <Input name='password' type='password' label='Password'/>
