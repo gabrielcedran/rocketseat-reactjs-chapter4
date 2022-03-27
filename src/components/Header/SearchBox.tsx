e0e6751429f9055bca9d26d9bc78ed761e3c1f78
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-27 11:16:27 +0100

    Controller vs uncontrolled components

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,16 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+    // this approach is known as controlled components - in case there is a side-effect while the text is being input it is important apply the concept of debounce 
+    //const [searchText, setSearchText] = useState('');
+
+    // uncontrolled components is an approach that the value of the component is accessed when necessary instead of proactively. It is achieved via ref hook  
+    
+    const searchInputRef = useRef<HTMLInputElement>(null)
+    
+
     return(
         <Flex
             as='label'
@@ -15,6 +24,10 @@ export function SearchBox() {
             borderRadius='full'
         >
             <Input 
+                // controlled component approach
+                //value={searchText}
+                //onChange={event => {setSearchText(event.target.value)}}
+                ref={searchInputRef}
                 color='gray.50'
                 variant='unstyled'
                 px='4'
